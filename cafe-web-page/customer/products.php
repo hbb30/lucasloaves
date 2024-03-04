397b59c68be0f72697f3a60b23e1a70e5c6f3438
Haries Bening <AbstractProgrammer@example.com>
2024-03-04 23:12:28 +1100

    commit all changes

--- cafe-web-page/customer/products.php
+++ cafe-web-page/customer/products.php
@@ -27,15 +27,26 @@ if ($stmt->rowCount() > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Selection</title>
+    <title>Products</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <style>
+        table{
+            background-color: white;
+            border-radius: 10px;
+        }
+
+        h1{
+            color: yellow;
+        }
+    </style>
 </head>
-<body>
+<body class="bg-dark">
     <div class="container mt-5">
-        <h1>Product Selection</h1>
+        <h1 class="text-warning mt-3"><span class="text-light">Manage</span> Products</h1>
         <table class="table table-bordered">
             <thead>
                 <tr>
