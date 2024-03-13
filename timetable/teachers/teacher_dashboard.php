760b49761e081a658e1a015bee1e1a4e8e6d98e0
Haries Bening <AbstractProgrammer@example.com>
2024-03-13 13:41:22 +1100

    commit all changes

--- timetable/teachers/teacher_dashboard.php
+++ timetable/teachers/teacher_dashboard.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Personal Address Book</title>
+  <title>My Timetable</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <style>
     .content-container {
@@ -14,11 +14,11 @@
 </head>
 <body class="bg-white">
 
-<?php include '../sidebar.php'; ?>
+<?php include 'sidebar.php'; ?>
   
 <div class="container content-container">
     <div class="d-flex justify-content-between">
-        <h1 class="text-warning mt-3"><span class="text-dark">Personal Address</span> Book</h1>
+        <h1 class="text-warning mt-3"><span class="text-dark">My</span> Timetable</h1>
         <button type="button" class="btn btn-outline-light mt-3"  data-bs-toggle="modal" data-bs-target="#log_out">
         Logout &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
@@ -29,23 +29,6 @@
     
     <div class="p-4 shadow bg-light mt-3 rounded-4 ">
         <div class="d-flex justify-content-between gap-3 mb-4">
-            <div>
-                <!-- button add -->
-                <button type="button" class="btn btn-warning rounded-3 shadow" data-bs-toggle="modal" data-bs-target="#add_user">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                    </svg>
-                </button>
-                <!-- button print -->
-                <a href="report/userReport.php" target="_blank" class="btn btn-warning rounded-3 shadow ms-1" id="printUser" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
-                        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
-                        <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
-                    </svg>
-                </a>
-
-            </div>
             
             <div class="input-group w-25">
                 <input type="text" class="form-control rounded-3" id="searchUser" placeholder="Search Name">
@@ -62,10 +45,9 @@
         <table class="table ">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Userlevel</th>
-                    <th scope="col">Action</th>
+                    <th scope="col">Course Title</th>
+                    <th scope="col">Room</th>
+                    <th scope="col">Schedule</th>
                 </tr>
             </thead>
             <tbody id="showUser">
@@ -81,11 +63,6 @@
   include the add_user from the modal folder 
   meaning, all codes in add_user.php will be included here       
 -->
-
-<?php 
-include("modals/manageUser.php");
-include("modals/alertModal.php");
-?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
