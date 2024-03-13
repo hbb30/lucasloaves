a17dd79f9c6fcbc985a8b93b71cf099a46f6d87e
Haries Bening <AbstractProgrammer@example.com>
2024-03-13 13:23:49 +1100

    commit all changes

--- timetable/admin/coursemanagement/manage_course.php
+++ timetable/admin/coursemanagement/manage_course.php
@@ -93,7 +93,7 @@ include("modals/alertModal.php");
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- script is in JS folder so that the code is cleaner and sorted -->
 <script src="adminJs/manageUser.js"></script>
-<script src="../systemJs/confirmLogout.js"></script>
+<script src="../../systemJs/confirmLogout.js"></script>
 <script>
   
 </script>
